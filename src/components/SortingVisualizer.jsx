86f160dcc12aeb048b0f53ba97d72d25de5ef6bb
mukhamediyar <mukhamediyar@berkeley.edu>
2022-09-05 17:00:07 -0700

    visuals

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -7,7 +7,8 @@ import selectionSort from "../sorting/selectionSort";
 const SortingVisualizer = () => {
   const [numItems, setNumItems] = useState(25);
   // const [delay, setDelay] = useState(500);
-  let delay = 200;
+  let delay = 50;
+  let maxVal = 100;
   const [i, seti] = useState(1);
   const [timeOuts, setTimeOuts] = useState([]);
 
@@ -16,6 +17,7 @@ const SortingVisualizer = () => {
     for (let i = 0; i < items; i++) {
       arr.push(i);
     }
+    maxVal = Math.max(...arr);
     return arr;
   };
 
@@ -57,7 +59,7 @@ const SortingVisualizer = () => {
       </button>
       <div className="barHolder">
         {itemsArray.map((item) => (
-          <h1 className="bar" key={item}>{item}</h1>
+          <h1 style={{height: `${(item / maxVal) * 90 + 10}%`}} className="bar" key={item}>{item}</h1>
         ))}
       </div>
       <div className="btnnHolder">
